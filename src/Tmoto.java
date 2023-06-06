a3900751707a765a484f0ac3f25849d94d9e09f3
Dantesuito <deadblood404@hotmail.com>
2023-06-06 19:38:01 +0200

    Test de frenar creado Test de frenar fallado. Creado método frenar Test de frenar fallado Fallo solucionado Test de frenar pasado

--- src/Tmoto.java
+++ src/Tmoto.java
@@ -40,4 +40,8 @@ public class Tmoto {
     public void acelerar(int i) {
         velActual += i;
     }
+
+    public void frenar(int i) {
+        velActual -= i;
+    }
 }
--- tests/TestMoto.java
+++ tests/TestMoto.java
@@ -3,9 +3,17 @@ import org.junit.jupiter.api.Test;
 
 public class TestMoto {
     @Test
-    public void test_al_acelerar_moto_su_velocidad_aumenta(){
+    public void test_al_acelerar_moto_su_velocidad_aumenta_DanielVitalTorres(){
         Tmoto moto = new Tmoto();
         moto.acelerar(30);
         Assertions.assertEquals(30, moto.getVelActual());
+    }
+
+    @Test
+    public void test_al_frenar_moto_su_velocidad_disminuye_DanielVitalTorres(){
+        Tmoto moto = new Tmoto();
+        moto.setVelActual(40);
+        moto.frenar(30);
+        Assertions.assertEquals(10, moto.getVelActual());
     }
 }
